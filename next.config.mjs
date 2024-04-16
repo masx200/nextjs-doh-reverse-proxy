9114e04cecc316c4a669d495f37335e1d5403f04
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-16 17:30:48 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,44 @@
 import withPWA from "next-pwa";
+//@ts-ignore
 import runtimeCaching from "next-pwa/cache.js";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 // @ts-check
-"use strict";
+("use strict");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        // Warning: Dangerously allow production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
-    poweredByHeader: false,
-    webpack: (
-        config,
-        { dev }, // { buildId, dev, isServer, defaultLoaders, webpack, ...rest }
-    ) => {
-        // console.log({
-        //     buildId,
-        //     dev,
-        //     isServer,
-        //     defaultLoaders,
-        //     webpack,
-        //     ...rest,
-        // });
-        const ftcwp = new ForkTsCheckerWebpackPlugin();
-        config.plugins = [...config.plugins, ftcwp];
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  poweredByHeader: false,
+  webpack: (
+    config,
+    { dev } // { buildId, dev, isServer, defaultLoaders, webpack, ...rest }
+  ) => {
+    // console.log({
+    //     buildId,
+    //     dev,
+    //     isServer,
+    //     defaultLoaders,
+    //     webpack,
+    //     ...rest,
+    // });
+    const ftcwp = new ForkTsCheckerWebpackPlugin();
+    config.plugins = [...config.plugins, ftcwp];
 
-        if (!dev) {
-            config.optimization.minimize = true;
-        }
-        // console.log({ config });
-        return config;
-    },
+    if (!dev) {
+      config.optimization.minimize = true;
+    }
+    // console.log({ config });
+    return config;
+  },
 };
 const config = withPWA({
-    register: true,
-    disable: process.env.NODE_ENV === "development",
-    dest: "public",
-    runtimeCaching,
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+  runtimeCaching,
 })(nextConfig);
 export default config;
